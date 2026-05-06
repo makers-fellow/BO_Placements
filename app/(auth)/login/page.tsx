23726ede03573fb945514e8221e28221870bc21a
Andy <andresreinoza1@gmail.com>
2026-05-05 21:48:13 -0300

    fix: add spacing between password input and login button

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -64,7 +64,7 @@ export default function LoginPage() {
             />
           </div>
         </CardContent>
-        <CardFooter className="flex flex-col gap-4">
+        <CardFooter className="flex flex-col gap-4 pt-6">
           <Button 
             className="w-full bg-[#86EFAC] text-[#0F1729] hover:bg-[#86EFAC]/90 font-semibold" 
             type="submit" 
